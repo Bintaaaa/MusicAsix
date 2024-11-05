c1776a5eff6914ef7429601da7a7dfd369bdb76b
Bintaaaa <bijantium.yt.32@gmail.com>
2024-11-05 13:06:53 +0700

    Initial Commit

---
+++ MusicAsixTests/MusicAsixTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAsixTests.swift
+//  MusicAsixTests
+//
+//  Created by Bijantyum on 05/11/24.
+//
+
+import Testing
+@testable import MusicAsix
+
+struct MusicAsixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
