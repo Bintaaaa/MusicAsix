e9cc85a1bca4e59228550401a5bcbab9c1472df3
Bintaaaa <bijantium.yt.32@gmail.com>
2024-11-05 19:48:27 +0700

    feat: add music remote datasource

---
+++ MusicAsix/AppSource/Data/Models/MusicModelDTO.swift
@@ -0,0 +1,28 @@
+//
+//  MusicModelDTO.swift
+//  MusicAsix
+//
+//  Created by Bijantyum on 05/11/24.
+//
+import Foundation
+
+struct SearchDTO: Decodable {
+    let results: [MusicModelDTO]
+}
+
+struct MusicModelDTO: Codable {
+    let artistID, collectionID, trackID: Int?
+    let artistName, collectionName, trackName, collectionCensoredName: String?
+    let trackCensoredName: String?
+    let artistViewURL, collectionViewURL, trackViewURL: String?
+    let previewURL: String?
+    let artworkUrl30, artworkUrl60, artworkUrl100: String?
+    let collectionPrice, trackPrice: Double?
+    let discCount, discNumber, trackCount, trackNumber: Int?
+    let trackTimeMillis: Int?
+    let primaryGenreName: String?
+    let isStreamable: Bool?
+    let contentAdvisoryRating: String?
+    let collectionArtistID: Int?
+    let collectionArtistName: String?
+}
